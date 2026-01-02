8d33ee281c94d4cd9a745558cfef145cf1a9410e
noyeosa-obazee <obazeenoyeosa@gmail.com>
2026-01-02 15:07:02 +0100

    fix: serve html files based on request instead of hard coded text

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const http = require("node:http");
+const fs = require("fs");
+
+const home = fs.readFileSync("index.html", "utf-8");
+const about = fs.readFileSync("about.html", "utf-8");
+const contactMe = fs.readFileSync("contact-me.html", "utf-8");
+const page404 = fs.readFileSync("404.html", "utf-8");
 
 http
   .createServer(function (req, res) {
+    console.log("Request received for:", req.url);
     if (req.url === "/") {
       res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("The Home Page");
+      res.end(home);
     } else if (req.url === "/about") {
       res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("The About Page");
+      res.end(about);
     } else if (req.url === "/contact-me") {
       res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("How to Contact me");
+      res.end(contactMe);
     } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("Page not Found - 404");
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end(page404);
     }
   })
-  .listen(8080, () => console.log("Server is running on port 8000"));
+  .listen(8080, () => console.log("Server is running on port 8080"));
